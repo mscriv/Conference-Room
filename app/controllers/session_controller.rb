51355794f92e86c6a7c58ebd201077530260f3d0
Michael Scriven <bigscriv03@gmail.com>
2014-11-11 16:56:22 -0500

    Completed 47

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -10,9 +10,11 @@ class SessionController < ApplicationController
 
       session[:user_id] = user.id
       flash[:notice] = 'You have successfully signed up.'
+
+      redirect_to rooms_path
+
     else
       flash[:error] = "We were unable to sign you up. #{user.errors.full_messages.join('. ')}."
-
     end
   end
 
